dd42816aff6e1b16233ca9df94deb1af8b9ab28e
MichaelPooleLF <michael.johnpoole@gmail.com>
2020-09-16 14:48:05 -0500

    added product list component

---
+++ client/components/product-list.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+// import ProductListItems from './product-list-items';
+
+class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
+
+  render() {
+    return (
+      <>
+      </>
+    );
+  }
+}
+
+export default ProductList;
