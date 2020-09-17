7a0d98cd57cf3aa2473ccac559f6f9da91c8196a
MichaelPooleLF <michael.johnpoole@gmail.com>
2020-09-17 15:30:46 -0500

    finished post and get endpoints for cart

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ app.get('/api/products/:productId', (req, res, next) => {
   const productId = parseInt(req.params.productId, 10);
   if (!Number.isInteger(productId) || productId <= 0) {
     return res.status(400).json({
-      error: 'productId munst be a positive integer'
+      error: 'productId must be a positive integer'
     });
   }
   const sql = `
@@ -53,10 +53,113 @@ app.get('/api/products/:productId', (req, res, next) => {
       if (!product) {
         next();
       } else {
-        res.status(200).json(result.rows[0]);
+        res.status(200).json(product);
       }
     })
     .catch(err => { next(err); });
+});
+
+app.get('/api/cart', (req, res, next) => {
+  if (!req.session.cartId) {
+    return [];
+  } else {
+    const sql = `
+    select "c"."cartItemId",
+           "c"."price",
+           "p"."productId",
+           "p"."image",
+           "p"."name",
+           "p"."shortDescription"
+      from "cartItems" as "c"
+      join "products" as "p" using("productId")
+     where "c"."cartId" = $1
+    `;
+    const params = [req.session.cartId];
+    db.query(sql, params)
+      .then(result => {
+        res.status(200).json(result.rows);
+      })
+      .catch(err => next(err));
+  }
+  // db.query('select * from "carts"')
+  //   .then(result => {
+  //     res.json(result.rows);
+  //   })
+  //   .catch(err => next(err));
+});
+
+app.post('/api/cart', (req, res, next) => {
+  const productId = parseInt(req.body.productId, 10);
+  if (!Number.isInteger(productId) || productId <= 0) {
+    return res.status(400).json({
+      error: 'productId must be a positive integer'
+    });
+  }
+  const sql = `
+    select "price"
+      from "products"
+     where "productId" = $1
+  `;
+  const params = [productId];
+  db.query(sql, params)
+    .then(result => {
+      const price = result.rows[0];
+      if (!price) {
+        throw next(new ClientError('That product doesn\'t exist. Please enter a different productId', 404));
+      } else if (req.session.cartId) {
+        return {
+          cartId: req.session.cartId,
+          price: price.price
+        };
+      } else {
+        const cartSQL = `
+          insert into "carts" ("cartId", "createdAt")
+            values (default, default)
+            returning "cartId"
+        `;
+        return db.query(cartSQL)
+          // .then(result => res.json(result))
+          .then(result => {
+            const cartId = result.rows[0].cartId;
+            return {
+              cartId,
+              price: price.price
+            };
+          })
+          .catch(err => next(err));
+      }
+    })
+    .then(result => {
+      req.session.cartId = result.cartId;
+      const cartItemSQL = `
+        insert into "cartItems" ("cartId", "productId", "price")
+          values ($1, $2, $3)
+          returning "cartItemId"
+      `;
+      const params = [req.session.cartId, productId, result.price];
+      return db.query(cartItemSQL, params)
+        .then(result => result.rows[0].cartItemId)
+        .catch(err => next(err));
+    })
+    .then(result => {
+      const cartItemId = result;
+      const productInfoSQL = `
+        select "c"."cartItemId",
+               "c"."price",
+               "p"."productId",
+               "p"."image",
+               "p"."name",
+               "p"."shortDescription"
+          from "cartItems" as "c"
+          join "products" as "p" using ("productId")
+         where "c"."cartItemId" = $1
+      `;
+      const params = [cartItemId];
+      return db.query(productInfoSQL, params)
+        .then(result => res.status(201).json(result.rows[0]))
+        .catch(err => next(err));
+    })
+    .catch(err => next(err));
 });
 
 // serverside error handling
