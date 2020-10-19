a0871b100df1dd87f65bfdb5aed68e18397ba69c
MichaelPooleLF <michael.johnpoole@gmail.com>
2020-10-19 14:00:18 -0500

    added hover affect on items

--- client/components/product-list-items.jsx
+++ client/components/product-list-items.jsx
@@ -9,7 +9,7 @@ function ProductListItem(props) {
 
   return (
     <div onClick={props.handleClick} className="col-sm-12 col-md-4 mt-5 card-group" id={props.product.productId}>
-      <div className="card pointer bg-shadow">
+      <div className="card pointer bg-shadow list-item">
         <div className="image">
           <img src={props.product.image} className="card-img-top"/>
         </div>
